f948088836fcc3ad62be58b8208846f8b5a5ea7b
samwondim <samwondim@gmail.com>
2024-05-23 14:17:45 +0300

    added additional functions

--- starter.py
+++ starter.py
@@ -9,4 +9,15 @@ def process_email(text):
     return list(set(text.split()))
 
 
+def predict_naive_bayes(word):
+    pass
+
+
+def calculate_posterior(emails):
+    """This function should return the probability if an email contains the
+    word 'lottery.'
+    """
+    pass
+
+
 emails["words"] = emails["text"].apply(process_email)
